291fbb0a6fb0a0b0b7b0ff997d8a80e626f4457e
diego <dgarcia@triploop.com>
2021-04-26 09:32:47 -0600

    removed nfc

--- uni_links/ios/Classes/UniLinksPlugin.h
+++ uni_links/ios/Classes/UniLinksPlugin.h
@@ -1,5 +1,4 @@
 #import <Flutter/Flutter.h>
-#import <CoreNFC/CoreNFC.h>
 
 
 @protocol UriDelegate <NSObject>
@@ -12,13 +11,3 @@
     continueUserActivity:(NSUserActivity * _Nullable)userActivity
       restorationHandler:(void (^_Nullable)(NSArray *_Nullable))restorationHandler;
 @end
-
-API_AVAILABLE(ios(11.0)) API_UNAVAILABLE(macos, watchos, tvos)
-@interface NFCCallbacks : NSObject <NFCNDEFReaderSessionDelegate>
-@property(nonatomic, weak) _Nullable id <UriDelegate> delegate;
-@property(nonatomic, copy) NSString* _Nullable dialogMessage;
-@property(nonatomic, strong) NFCNDEFReaderSession * _Nullable nfcReaderSession;
-- (void) start;
-- (void) stop;
-@end
-
